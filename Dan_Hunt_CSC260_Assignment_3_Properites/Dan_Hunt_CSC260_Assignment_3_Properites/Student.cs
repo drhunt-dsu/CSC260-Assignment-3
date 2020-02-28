46361896f7a92a0835801b2d3527ed891ce18fb3
Dan Hunt <daniel.hunt@trojans.dsu.edu>
2020-02-27 22:42:55 -0600

    Added backing fields for logging portion of assignment

--- Dan_Hunt_CSC260_Assignment_3_Properites/Dan_Hunt_CSC260_Assignment_3_Properites/Student.cs
+++ Dan_Hunt_CSC260_Assignment_3_Properites/Dan_Hunt_CSC260_Assignment_3_Properites/Student.cs
@@ -11,31 +11,123 @@ namespace Dan_Hunt_CSC260_Assignment_3_Properites
 {
     class Student
     {
+        private readonly int _id;
+        private string _name;
+        private string _major;
+        private DateTime _startDate;
+        private DateTime _anticipatedGraduationDate;
+        private string _mailingAddress;
+        private string? _stateProvince;
+        private string _country;
+        private string _email;
+        private PhoneAttribute _phone;
+
         #region Properties
-        //These properties are auto-generated and implicitly include backing fields
-        public int Id { get; } //Read Only (no setter)
-        public string Name { get; set; }
-        public string Major { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime AnticipatedGraduationDate { get; set; }
-        public string MailingAddress { get; set; }
-        public string? StateProvince { get; set; }
-        public string Country { get; set; }
-        public string Email { get; set; }
-        public PhoneAttribute Phone { get; set; }
+        public int Id => _id;//Read Only (no setter)
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                Console.WriteLine($"Student {Id} _name set to {value}");
+            }
+        }
+
+        public string Major
+        {
+            get => _major;
+            set
+            {
+                _major = value;
+                Console.WriteLine($"Student {Id} _major set to {value}");
+            }
+        }
+
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set
+            {
+                _startDate = value;
+                Console.WriteLine($"Student {Id} _startDate set to {value}");
+            }
+        }
+
+        public DateTime AnticipatedGraduationDate
+        {
+            get => _anticipatedGraduationDate;
+            set
+            {
+                _anticipatedGraduationDate = value;
+                Console.WriteLine($"Student {Id} _anticipatedGraduationDate set to {value}");
+            }
+        }
+
+        public string MailingAddress
+        {
+            get => _mailingAddress;
+            set
+            {
+                _mailingAddress = value;
+                Console.WriteLine($"Student {Id} _mailingAddress set to {value}");
+            }
+        }
+
+        public string? StateProvince
+        {
+            get => _stateProvince;
+            set
+            {
+                _stateProvince = value;
+                Console.WriteLine($"Student {Id} _stateProvince set to {value}");
+            }
+        }
+
+        public string Country
+        {
+            get => _country;
+            set
+            {
+                _country = value;
+                Console.WriteLine($"Student {Id} _country set to {value}");
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                Console.WriteLine($"Student {Id} _email set to {value}");
+            } 
+        }
+
+        public PhoneAttribute Phone
+        {
+            get => _phone;
+            set
+            {
+                _phone = value;
+                Console.WriteLine($"Student {Id} _phone set to {value}");
+            }
+        }
+
         #endregion
 
         //default constructor
         public Student()
         {
-            Id = -1;
+            _id= -1;
             Name = "John Doe";
         }
 
         //parameterized constructor
         public Student(int id)
         {
-            Id = id;
+            _id = id;
         }
 
         /*Instructions:
